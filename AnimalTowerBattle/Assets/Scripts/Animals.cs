ecf3fb7cd26e84559a59f7b9aecdf789741ca4ce
Logy <svizra8019@gmail.com>
2017-12-11 00:47:00 +0900

    ポリゴンをきれいに張り直しました！！！！

--- AnimalTowerBattle/Assets/Scripts/Animals.cs
+++ AnimalTowerBattle/Assets/Scripts/Animals.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Animals : MonoBehaviour
 {
 
-    public GameObject AnimalPrefab;
+    public GameObject[] AnimalPrefab;
     public Sprite[] AnimalSprites;
 
     void Start()
@@ -17,10 +17,10 @@ public class Animals : MonoBehaviour
         for (int i = 0; i < count; i++)
         {
             Vector2 pos = new Vector2(Random.Range(-1.0f, 1.0f), 7f);
-            GameObject ball = Instantiate(AnimalPrefab, pos, Quaternion.AngleAxis(Random.Range(-40, 40), Vector3.forward)) as GameObject;
             int spriteId = Random.Range(0, 5);
-            ball.name = "Animal" + spriteId;
-            SpriteRenderer spriteObj = ball.GetComponent<SpriteRenderer>();
+            GameObject Animal = Instantiate(AnimalPrefab[spriteId], pos, Quaternion.AngleAxis(Random.Range(-40, 40), Vector3.forward)) as GameObject;
+            Animal.name = "Animal" + spriteId;
+            SpriteRenderer spriteObj = Animal.GetComponent<SpriteRenderer>();
             spriteObj.sprite = AnimalSprites[spriteId];
             yield return new WaitForSeconds(2);
         }
